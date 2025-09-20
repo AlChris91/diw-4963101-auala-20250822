122eb4ba3bf6d46f8f595cb92bf84bd85efd7079
Álan Christian <xalanchristian@outlook.com>
2025-09-20 01:25:16 +0000

    criado login

---
+++ bd.js
@@ -0,0 +1,37 @@
+const usuario = {
+    email: "admin@email.com",
+    senha: "admin"
+};
+
+const dados = [
+    {
+        id: ,
+        titulo: ,
+        descricao: ,
+        conteudo: ,
+        categoria: ,
+        autor: ,
+        data: ,
+        imagem: "./asset/imagem"
+    },
+    {
+        id: ,
+        titulo: ,
+        descricao: ,
+        conteudo: ,
+        categoria: ,
+        autor: ,
+        data: ,
+        imagem: "./asset/imagem"
+    },
+    {
+        id: ,
+        titulo: ,
+        descricao: ,
+        conteudo: ,
+        categoria: ,
+        autor: ,
+        data: ,
+        imagem: "./asset/imagem"
+    }
+];
---
+++ login.js
@@ -0,0 +1,21 @@
+const email = usuario.email;
+const senha = usuario.senha;
+
+function verificarCredenciais() {
+    const emailInformado = document.getElementById("email").value;
+    const senhaInformada = document.getElementById("senha").value
+
+    if (emailInformado === email) {
+        alert("Email informado corretamente");
+        if (senhaInformada === senha) {
+            // alert("Senha informada corretamente");
+            window.location = "home.html";
+        } else {
+            // alert("Senha incorreta");
+            
+        }
+    } else {
+        // alert("Email incorreto");
+    }
+
+}
