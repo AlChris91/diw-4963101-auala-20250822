21a6f84e66b300c14eea71611f51625259a02318
Álan Christian <xalanchristian@outlook.com>
2025-09-20 01:38:30 +0000

    atualizando dados

--- bd.js
+++ bd.js
@@ -1,37 +1,37 @@
 const usuario = {
-    email: "admin@email.com",
-    senha: "admin"
+    "email": "admin@email.com",
+    "senha": "admin"
 };
 
 const dados = [
     {
-        id: ,
-        titulo: ,
-        descricao: ,
-        conteudo: ,
-        categoria: ,
-        autor: ,
-        data: ,
-        imagem: "./asset/imagem"
+        "id": 1 ,
+       " titulo": "Mobilidade Urbana em Belo Horizonte",
+        "descricao": "Estudos para definição política e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana" ,
+        "conteudo":  ,
+        "categoria": "transporte" ,
+        "autor": "" ,
+        "data": ,
+        "imagem": "./assets/imagem/mobilidade.png"
     },
     {
-        id: ,
-        titulo: ,
-        descricao: ,
-        conteudo: ,
-        categoria: ,
-        autor: ,
-        data: ,
-        imagem: "./asset/imagem"
+        "id": 2 ,
+       " titulo": "Sexta geração de tecnologia de comunicação sem fio" ,
+        "descricao": ,
+       " conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "./assets/imagem/6g.png"
     },
     {
-        id: ,
-        titulo: ,
-        descricao: ,
-        conteudo: ,
-        categoria: ,
-        autor: ,
-        data: ,
-        imagem: "./asset/imagem"
+        "id": 3 ,
+        "titulo": "Festival de música de Belo Horizonte" ,
+        "descricao": ,
+        "conteudo": ,
+       " categoria": ,
+        "autor": ,
+       " data": ,
+        "imagem": "./assets/imagem/festival.png"
     }
 ];
