2a0dbf57b559467abfcf7629af59a432bd28eaeb
Álan Christian <xalanchristian@outlook.com>
2025-09-26 20:47:43 +0000

    Dados adicionados

--- bd.js
+++ bd.js
@@ -5,33 +5,33 @@ const usuario = {
 
 const dados = [
     {
-        "id": 1 ,
-       " titulo": "Mobilidade Urbana em Belo Horizonte",
-        "descricao": "Estudos para definição política e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana" ,
-        "conteudo":  ,
-        "categoria": "transporte" ,
-        "autor": "" ,
-        "data": ,
+          "id": 1,
+        "titulo": "Mobilidade Urbana em Belo Horizonte",
+        "descricao": "Estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana.",
+        "conteudo": "O Governo do Estado de Minas Gerais iniciou a elaboração do Plano de Mobilidade da Região Metropolitana de Belo Horizonte em outubro de 2018. O plano tem como objetivo apresentar estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana na RMBH e sua sustentabilidade. Ele terá como resultado a construção de planos de caráter multimodal para o Transporte Coletivo Metropolitano, Logística Urbana Metropolitana, Transporte individual motorizado e Transporte ativo. Serão considerados como temas transversais o uso e ocupação do solo, sustentabilidade, segurança viária, acessibilidade, governança e participação popular.",
+        "categoria": "Transporte",
+        "autor": "Ana Maria Silva",
+        "data": "19/09/2025",
         "imagem": "./assets/imagem/mobilidade.png"
     },
     {
         "id": 2 ,
        " titulo": "Sexta geração de tecnologia de comunicação sem fio" ,
-        "descricao": ,
-       " conteudo": ,
-        "categoria": ,
-        "autor": ,
-        "data": ,
+        "descricao": "Tecnologia 6G: diferenças e aplicações do futuro da internet",
+       " conteudo": "A tecnologia 6G é a próxima geração da comunicação sem fio, que virá depois da 5G, e promete revolucionar a maneira como interagimos com o mundo digital. Ainda em fase de pesquisa e desenvolvimento, ela irá expandir as capacidades da 5G, oferecendo velocidades de conexão muito mais mais rápidas (até 100 vezes mais velozes), latência ultrabaixa (microssegundos) e uma capacidade de rede massiva.",
+        "categoria": "Tecnologia" ,
+        "autor": "Digital Futurecom" ,
+        "data": "27/07/23",
         "imagem": "./assets/imagem/6g.png"
     },
     {
         "id": 3 ,
         "titulo": "Festival de música de Belo Horizonte" ,
-        "descricao": ,
-        "conteudo": ,
-       " categoria": ,
-        "autor": ,
-       " data": ,
+        "descricao":"História do Épop BH - Festival de Música Pop Autoral" ,
+        "conteudo": "O ÉPOP BH nasceu em 2022 com a missão de valorizar a música pop autoral brasileira e criar um espaço de visibilidade para compositores independentes. Realizado pelo Espaço Cênico Rick Alves em parceria com a ABAI (Associação Brasileira dos Artistas Independentes), o festival se consolida a cada edição como vitrine para novos talentos da música pop nacional." ,
+       " categoria":"Música" ,
+        "autor":"Épop BH" ,
+       " data": "01/10/25" ,
         "imagem": "./assets/imagem/festival.png"
     }
 ];
